9328ca77be53b92b8921876a29940a0d7e02e7a9
gastonfuentes <gastonnicolasfuentes@gmail.com>
2024-06-14 18:16:33 -0300

    funcion counter listo

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,17 +5,27 @@ const Counter = () => {
 
     const [counter, setCounter] = useState(0)
 
-    const incrementar = () => {
-        setCounter(counter + 1)
+    const incrementar = (numero: number = 1) => {
+        setCounter(counter + numero)
     }
+
+    /*  const reset = (reset:number) =>{
+         counter = 0
+     } */
 
     return (
         <div className='mt-5'>
             <h3>Counter: usestate</h3>
             <span>Valor: {counter}</span>
             <br />
-            <button className="btn btn-outline-primary mt-2" onClick={incrementar}>
+            <button className="btn btn-outline-primary mt-2" onClick={() => incrementar()}>
                 +1
+            </button>
+            <button className="btn btn-outline-primary mt-2" onClick={() => incrementar(2)}>
+                +2
+            </button>
+            <button className="btn btn-outline-danger mt-2" onClick={() => setCounter(0)}>
+                Reset
             </button>
         </div>
     )
